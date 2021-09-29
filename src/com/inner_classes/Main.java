3eaef5e9b0da381cb67d231064b9517eec78df05
upes1679 <88457937+upes1679@users.noreply.github.com>
2021-09-29 21:47:37 +0300

    Add files via upload

---
+++ src/com/inner_classes/Main.java
@@ -0,0 +1,19 @@
+package com.inner_classes;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    OuterClass outerClass=new OuterClass(10);
+	    outerClass.outerMethod();
+	    OuterClass.InnerClass innerClass= outerClass.new InnerClass(20);
+	    innerClass.innerMethod();
+
+	    outerClass.setInnerClass(innerClass);
+	    outerClass.getInnerClass().innerMethod();
+
+	    //------------------------------------------------------------------
+		OuterClassB.StaticInnerClass staticInnerClass=new OuterClassB.StaticInnerClass(10);
+		OuterClassB outerClassB=new OuterClassB(20,30);
+		staticInnerClass.innerMethod();
+	}
+}
